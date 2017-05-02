0359eb9cd627ef14b9208a0c7f325562ca9c4202
Thien Hoang <thienhoang@umail.ucsb.edu>
2017-05-01 18:16:05 -0700

    READY TO ROLL

---
+++ nachos/code/threads/rwlock.cc
@@ -0,0 +1,76 @@
+#include <semaphore.h>
+#include <pthread.h>
+#include "rwlock.h"
+
+RWLock::RWLock(){
+	#ifdef RWLOCK
+	pthread_cond_init(&this->writeCV, NULL);;
+	pthread_cond_init(&this->readCV, NULL);
+	this->AW = 0;
+	this->WW = 0;
+	this->AR = 0;
+	this->WR = 0;
+	#endif
+	pthread_mutex_init ( &this->lock, NULL);
+}
+
+RWLock::~RWLock(){
+	#ifdef RWLOCK
+		pthread_cond_destroy(&this->readCV);
+		pthread_cond_destroy(&this->writeCV);
+	#endif
+	pthread_mutex_destroy(&this->lock);
+}
+
+void RWLock::startRead(){
+	pthread_mutex_lock(&this->lock);
+	#ifdef RWLOCK
+		while ((this->AW + this->WW) > 0){
+			this->WR++;
+			pthread_cond_wait(&this->readCV, &this->lock);
+			this->WR--;			
+		}
+		this->AR++;
+		pthread_mutex_unlock(&this->lock);
+	#endif
+}
+
+void RWLock::doneRead(){
+	#ifdef RWLOCK
+		pthread_mutex_lock(&this->lock);
+		this->AR--;
+		if(this->AR == 0 && this->WW > 0)
+			pthread_cond_signal(&this->writeCV);
+		pthread_mutex_unlock(&this->lock);
+	#else
+		pthread_mutex_unlock(&this->lock);
+	#endif
+}
+
+void RWLock::startWrite(){
+	pthread_mutex_lock(&this->lock);
+	#ifdef RWLOCK
+		while(  (this->AW + this->AR) > 0){
+			this->WW++;
+			pthread_cond_wait(&this->writeCV, &this->lock);
+			this->WW--;
+		}
+		this->AW++;
+		pthread_mutex_unlock(&this->lock);
+	#endif
+}
+
+void RWLock::doneWrite(){
+	#ifdef RWLOCK
+		pthread_mutex_lock(&this->lock);
+		this->AW--;
+		if (this->WW > 0){
+			pthread_cond_signal(&this->writeCV);
+		} else if (this->WR > 0){
+			pthread_cond_broadcast(&this->readCV);
+		}
+		pthread_mutex_unlock(&this->lock);
+	#else
+		pthread_mutex_unlock(&this->lock);
+	#endif
+}
