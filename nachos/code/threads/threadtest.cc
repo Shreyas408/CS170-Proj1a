8a1b38fad7b99df96f687abe15bd17e57f9b7439
Shreyas Canchi Radhakrishna <shreyas@umail.ucsb.edu>
2017-05-01 19:03:55 -0700

    I think we're donw with Task 1?

--- nachos/code/threads/threadtest.cc
+++ nachos/code/threads/threadtest.cc
@@ -1,72 +1,63 @@
 // threadtest.cc
-//	Simple test case for the threads assignment.
+//        Simple test case for the threads assignment.
 //
-//	Create two threads, and have them context switch
-//	back and forth between themselves by calling Thread::Yield,
-//	to illustratethe inner workings of the thread system.
+//        Create two threads, and have them context switch
+//        back and forth between themselves by calling Thread::Yield,
+//        to illustratethe inner workings of the thread system.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
 // All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
+
 #include "copyright.h"
 #include "system.h"
-
+#include "synch.h"
+#include "hashchain.h"
+#include <iostream>
+using namespace std;
 // testnum is set in main.cc
-int testnum = 1;
+int testnum = 0;
+HashMap m;
 
-//----------------------------------------------------------------------
-// SimpleThread
-// 	Loop 5 times, yielding the CPU to another ready thread
-//	each iteration.
-//
-//	"which" is simply a number identifying the thread, for debugging
-//	purposes.
-//----------------------------------------------------------------------
+#if defined(CHANGED) && defined(THREADS)
 
-void
-SimpleThread(int which)
-{
-    int num;
+#define NUMKEYS 100
+/* Test operations on hash table */
 
-    for (num = 0; num < 5; num++) {
-        printf("*** thread %d looped %d times\n", which, num);
-        currentThread->Yield();
-    }
+/* Each thread will use this function to access the hashtable. You can use
+ * this to test your locking mechanism used to protect the hash table */
+void tfunc(int arg){
+
+
+
+
 }
 
-//----------------------------------------------------------------------
-// ThreadTest1
-// 	Set up a ping-pong between two threads, by forking a thread
-//	to call SimpleThread, and then calling SimpleThread ourselves.
-//----------------------------------------------------------------------
-
-void
-ThreadTest1()
+void ThreadTest1(int childThreadNum)
 {
     DEBUG('t', "Entering ThreadTest1");
-
+    // Prepare a new thread to be forked
     Thread *t = new Thread("forked thread");
-
-    t->Fork(SimpleThread, 1);
-    SimpleThread(0);
+    // 'Fork' the new thread, putting it on the ready queue, about to run tfunc
+    t->Fork(tfunc, childThreadNum);
 }
 
-//----------------------------------------------------------------------
-// ThreadTest
-// 	Invoke a test routine.
-//----------------------------------------------------------------------
 
-void
-ThreadTest()
+void ThreadTest(int numThreads)
 {
-    switch (testnum) {
-    case 1:
-        ThreadTest1();
-        break;
-    default:
-        printf("No test specified.\n");
-        break;
+    if(numThreads < 0)
+    {
+        printf("Num threads must be >= 0");
+        return;
     }
-}
+    // No data races for writing workingThreads because only the parent is running at this time
 
+    // Spawn N child threads
+    for(int i = 0; i < numThreads; i++)
+    {
+        ThreadTest1(i + 1); // Pass the child thread's number (i+1)
+    }
+
+}
+#endif
